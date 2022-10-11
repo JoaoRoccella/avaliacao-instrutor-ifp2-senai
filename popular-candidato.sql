bfd717ae7896634058311ce37acf393eb1b09cfb
Joao <roccella@gmail.com>
2022-10-11 15:24:46 -0300

    Inicial

---
+++ popular-candidato.sql
@@ -0,0 +1,5 @@
+/*
+-- Query: 
+-- Date: 2022-10-11 14:01
+*/
+INSERT INTO `` (`id`,`email`,`nome_candidato`,`rg`,`vaga_candidato`) VALUES (1,'josedascouves@senai.com.br','José das Couves','12.345.678-00',1);
---
+++ popular-vaga.sql
@@ -0,0 +1,5 @@
+/*
+-- Query: 
+-- Date: 2022-10-11 14:01
+*/
+INSERT INTO `` (`codigo`,`data`,`descricao`,`nome`,`salario`) VALUES (1,'31/12/2022 00:00:00','Vaga para Instrutor de Tecnologia em que o perfil profissional deve incluir conhecimentos em banco de dados e desenvolvimento de sistemas em Java.','Instrutor de Tecnologia',2500.00);
